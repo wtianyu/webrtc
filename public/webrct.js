8957de4deae796f6d9f894c8b4e1fcca07072f84
unknown <>
2017-04-05 15:43:48 +0800

    commit1

---
+++ public/webrct.js
@@ -0,0 +1,427 @@
+var WebRTC = function() {
+    var PeerConnection = (window.PeerConnection || window.webkitPeerConnection00 || window.webkitRTCPeerConnection || window.mozRTCPeerConnection);
+    var URL = (window.URL || window.webkitURL || window.msURL || window.oURL);
+    var getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    var nativeRTCIceCandidate = (window.mozRTCIceCandidate || window.RTCIceCandidate);
+    var nativeRTCSessionDescription = (window.mozRTCSessionDescription || window.RTCSessionDescription); // order is very important: "RTCSessionDescription" defined in Nighly but useless
+    var moz = !!navigator.mozGetUserMedia;
+    var iceServer = {
+        "iceServers": [{
+            "url": "stun:stun.l.google.com:19302"
+        }]
+    };
+    var packetSize = 1000;
+
+
+    /**********************************************************/
+    /*                                                        */
+    /*                       事件处理器                       */
+    /*                                                        */
+    /**********************************************************/
+    function EventEmitter() {
+        this.events = {};
+    }
+    //绑定事件函数
+    EventEmitter.prototype.on = function(eventName, callback) {
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(callback);
+    };
+    //触发事件函数
+    EventEmitter.prototype.emit = function(eventName, _) {
+        var events = this.events[eventName],
+            args = Array.prototype.slice.call(arguments, 1),
+            i, m;
+
+        if (!events) {
+            return;
+        }
+        for (i = 0, m = events.length; i < m; i++) {
+            events[i].apply(null, args);
+        }
+    };
+
+
+    /**********************************************************/
+    /*                                                        */
+    /*                   流及信道建立部分                     */
+    /*                                                        */
+    /**********************************************************/
+
+
+    /*******************基础部分*********************/
+    function webrtc() {
+        //本地media stream
+        this.localMediaStream = null;
+        //所在房间
+        this.room = "";
+        //接收文件时用于暂存接收文件
+        this.fileData = {};
+        //本地WebSocket连接
+        this.web_socket = null;
+        //本地web_socket的id，由后台服务器创建
+        this.me = null;
+        //保存所有与本地相连的peer connection， 键为web_socket id，值为PeerConnection类型
+        this.peerConnections = {};
+        //保存所有与本地连接的web_socket的id
+        this.connections = [];
+        //初始时需要构建链接的数目
+        this.numStreams = 0;
+        //初始时已经连接的数目
+        this.initializedStreams = 0;
+        //保存所有的data channel，键为web_socket id，值通过PeerConnection实例的createChannel创建
+        this.dataChannels = {};
+        //保存所有发文件的data channel及其发文件状态
+        this.fileChannels = {};
+        //保存所有接受到的文件
+        this.receiveFiles = {};
+    }
+    //继承自事件处理器，提供绑定事件和触发事件的功能
+    webrtc.prototype = new EventEmitter();
+
+    //请求连接
+    webrtc.prototype.connect = function(server, room) {
+        var web_socket, that = this;
+        room = room || "";
+        web_socket = this.web_socket = new WebSocket(server);
+        console.log("正在建立连接...");
+        //WebSocket的初始化
+        web_socket.onopen = function() {
+            console.log("正在打开socket连接...");
+            web_socket.send(JSON.stringify({
+                "eventName": "__join",
+                "data": {
+                    "room": room
+                }
+            }));
+            that.emit("socket_opened", web_socket);
+        }
+
+        web_socket.onmessage = function(message) {
+            var json = JSON.parse(message.data);
+            if (json.eventName) {
+                console.log(json.eventName);
+                that.emit(json.eventName, json.data);
+            } else {
+                that.emit("socket_receive_message", web_socket, json);
+            }
+        };
+
+        web_socket.onerror = function(error) {
+            that.emit("socket_error", error, web_socket);
+        };
+
+        web_socket.onclose = function(data) {
+            that.localMediaStream.close();
+            var pcs = that.peerConnections;
+            for (i = pcs.length; i--;) {
+                that.closePeerConnection(pcs[i]);
+            }
+            that.peerConnections = [];
+            that.dataChannels = {};
+            that.fileChannels = {};
+            that.connections = [];
+            that.fileData = {};
+            that.emit('socket_closed', web_socket);
+        };
+
+        this.on('_peers', function(data) {
+            //获取所有服务器上的
+            //通过DataChannel的onmessage事件自动调用_peers
+            that.connections = data.connections;
+            that.me = data.you;
+            //发送获取连接广播
+            that.emit("get_peers", that.connections);
+            //发送连接操作
+            that.emit('connected', web_socket);
+        });
+
+        this.on("_ice_candidate", function(data) {
+            var candidate = new nativeRTCIceCandidate(data);
+            var pc = that.peerConnections[data.socketId];
+            pc.addIceCandidate(candidate);
+            that.emit('get_ice_candidate', candidate);
+        });
+
+        this.on('_new_peer', function(data) {
+            that.connections.push(data.socketId);
+            var pc = that.createPeerConnection(data.socketId),
+                i, m;
+            pc.addStream(that.localMediaStream);
+            that.emit('new_peer', data.socketId);
+        });
+
+        this.on('_remove_peer', function(data) {
+            var sendId;
+            that.closePeerConnection(that.peerConnections[data.socketId]);
+            delete that.peerConnections[data.socketId];
+            delete that.dataChannels[data.socketId];
+            for (sendId in that.fileChannels[data.socketId]) {
+                that.emit("send_file_error", new Error("Connection has been closed"), data.socketId, sendId, that.fileChannels[data.socketId][sendId].file);
+            }
+            delete that.fileChannels[data.socketId];
+            that.emit("remove_peer", data.socketId);
+        });
+
+        this.on('_offer', function(data) {
+            that.emit("get_offer", data);
+            that.receiveOffer(data.socketId, data.sdp);
+        });
+
+        this.on('_answer', function(data) {
+            that.receiveAnswer(data.socketId, data.sdp);
+            that.emit('get_answer', data);
+        });
+
+        this.on('send_file_error', function(error, socketId, sendId, file) {
+            that.cleanSendFile(sendId, socketId);
+        });
+
+        this.on('receive_file_error', function(error, sendId) {
+            that.cleanReceiveFile(sendId);
+        });
+
+        this.on('ready', function() {
+            console.log("正在初始化peerConnection");
+            that.createPeerConnections();
+            console.log("正在添加流监听");
+            that.addStreams();
+            console.log("正在建立数据通道");
+            that.addDataChannels();
+            console.log("发送offer类型的信令");
+            that.sendOffers();
+        });
+
+    }
+
+    webrtc.prototype.createStream = function(options) {
+        var that = this;
+        if (getUserMedia) {
+            this.numStreams++;
+            getUserMedia.call(navigator, options, function(stream) {
+                    that.localMediaStream = stream;
+                    that.initializedStreams++;
+                    that.emit("stream_created", stream);
+                    if (that.initializedStreams === that.numStreams) {
+                        that.emit("ready");
+                    }
+                },
+                function(error) {
+                    that.emit("stream_create_error111", error);
+                });
+        } else {
+            that.emit("stream_create_error", new Error('WebRTC is not yet supported in this browser.'));
+        }
+    };
+
+    //接收到Offer类型信令后作为回应返回answer类型信令
+    webrtc.prototype.receiveOffer = function(socketId, sdp) {
+        var pc = this.peerConnections[socketId];
+        this.sendAnswer(socketId, sdp);
+    };
+
+    //发送answer类型信令
+    webrtc.prototype.sendAnswer = function(socketId, sdp) {
+        var pc = this.peerConnections[socketId];
+        var that = this;
+        pc.setRemoteDescription(new nativeRTCSessionDescription(sdp));
+        pc.createAnswer(function(session_desc) {
+            pc.setLocalDescription(session_desc);
+            that.web_socket.send(JSON.stringify({
+                "eventName": "__answer",
+                "data": {
+                    "socketId": socketId,
+                    "sdp": session_desc
+                }
+            }));
+        }, function(error) {
+            console.log(error);
+        });
+    };
+
+    //接收到answer类型信令后将对方的session描述写入PeerConnection中
+    webrtc.prototype.receiveAnswer = function(socketId, sdp) {
+        var pc = this.peerConnections[socketId];
+        pc.setRemoteDescription(new nativeRTCSessionDescription(sdp));
+    };
+
+    webrtc.prototype.attachStream = function(stream, domId) {
+        var element = document.getElementById(domId);
+        if (navigator.mozGetUserMedia) {
+            element.mozSrcObject = stream;
+            element.play();
+        } else {
+            element.src = URL.createObjectURL(stream);
+        }
+        // element.src = URL.createObjectURL(stream);
+        console.log("实现视频流的输出");
+    };
+
+    //创建与其他用户连接的PeerConnections
+    webrtc.prototype.createPeerConnections = function() {
+        var i, m;
+        for (i = 0, m = this.connections.length; i < m; i++) {
+            this.createPeerConnection(this.connections[i]);
+        }
+    };
+
+    //创建单个PeerConnection
+    webrtc.prototype.createPeerConnection = function(socketId) {
+        var that = this;
+        var pc = new PeerConnection(iceServer);
+        this.peerConnections[socketId] = pc;
+        pc.onicecandidate = function(evt) {
+            if (evt.candidate)
+                that.web_socket.send(JSON.stringify({
+                    "eventName": "__ice_candidate",
+                    "data": {
+                        "label": evt.candidate.sdpMLineIndex,
+                        "candidate": evt.candidate.candidate,
+                        "socketId": socketId
+
+                    }
+                }));
+            that.emit("pc_get_ice_candidate", evt.candidate, socketId, pc);
+        };
+
+        pc.onopen = function() {
+            that.emit("pc_opened", socketId, pc);
+            console.log("哇哈哈哈哈");
+        };
+
+        pc.onaddstream = function(evt) {
+            that.emit('pc_add_stream', evt.stream, socketId, pc);
+        };
+
+        pc.ondatachannel = function(evt) {
+            that.addDataChannel(socketId, evt.channel);
+            that.emit('pc_add_data_channel', evt.channel, socketId, pc);
+        };
+        return pc;
+    };
+
+    //将本地流添加到所有的PeerConnection实例中
+    webrtc.prototype.addStreams = function() {
+        var i, m,
+            stream,
+            connection;
+        for (connection in this.peerConnections) {
+            this.peerConnections[connection].addStream(this.localMediaStream);
+        }
+    };
+
+    //对所有的PeerConnections创建Data channel
+    webrtc.prototype.addDataChannels = function() {
+        var connection;
+        for (connection in this.peerConnections) {
+            this.createDataChannel(connection);
+        }
+    };
+
+    //对某一个PeerConnection创建Data channel
+    webrtc.prototype.createDataChannel = function(socketId, label) {
+
+        var pc, key, channel;
+        pc = this.peerConnections[socketId];
+
+        if (!socketId) {
+            this.emit("data_channel_create_error", socketId, new Error("attempt to create data channel without socket id"));
+        }
+
+        if (!(pc instanceof PeerConnection)) {
+            this.emit("data_channel_create_error", socketId, new Error("attempt to create data channel without peerConnection"));
+        }
+        try {
+            channel = pc.createDataChannel(label);
+        } catch (error) {
+            this.emit("data_channel_create_error", socketId, error);
+        }
+        return this.addDataChannel(socketId, channel);
+    };
+
+    //为Data channel绑定相应的事件回调函数
+    webrtc.prototype.addDataChannel = function(socketId, channel) {
+        var that = this;
+        channel.onopen = function() {
+            that.emit('data_channel_opened', channel, socketId);
+        };
+
+        channel.onclose = function(event) {
+            delete that.dataChannels[socketId];
+            that.emit('data_channel_closed', channel, socketId);
+        };
+
+        channel.onmessage = function(message) {
+            var json;
+            json = JSON.parse(message.data);
+            console.log("000"+json.type);
+            console.log("111"+json.data);
+            if (json.type === '__file') {
+                /*that.receiveFileChunk(json);*/
+                that.parseFilePacket(json, socketId);
+            } else {
+                that.emit('data_channel_message', channel, socketId, json.data);
+            }
+        };
+
+        channel.onerror = function(err) {
+            that.emit('data_channel_error', channel, socketId, err);
+        };
+
+        this.dataChannels[socketId] = channel;
+        return channel;
+    };
+
+    //向所有PeerConnection发送Offer类型信令
+    webrtc.prototype.sendOffers = function() {
+        var i, m,
+            pc,
+            that = this,
+            pcCreateOfferCbGen = function(pc, socketId) {
+                return function(session_desc) {
+                    pc.setLocalDescription(session_desc);
+                    that.web_socket.send(JSON.stringify({
+                        "eventName": "__offer",
+                        "data": {
+                            "sdp": session_desc,
+                            "socketId": socketId
+                        }
+                    }));
+                };
+            },
+            pcCreateOfferErrorCb = function(error) {
+                console.log(error);
+            };
+        for (i = 0, m = this.connections.length; i < m; i++) {
+            pc = this.peerConnections[this.connections[i]];
+            pc.createOffer(pcCreateOfferCbGen(pc, this.connections[i]), pcCreateOfferErrorCb);
+        }
+        that.emit('initOver', "88888888");
+    };
+
+    //message
+    //消息广播
+    webrtc.prototype.broadcast = function(message) {
+        var socketId;
+        for (socketId in this.dataChannels) {
+            this.sendMessage(message, socketId);
+        }
+    };
+
+    //发送消息方法
+    webrtc.prototype.sendMessage = function(message, socketId) {
+       // if (this.dataChannels[socketId].readyState.toLowerCase() === "open") {
+            console.log("发送消息"+message);
+            this.dataChannels[socketId].send(JSON.stringify({
+                type: "__msg",
+                data: message
+            }));
+       // }
+    };
+
+    webrtc.prototype.closePeerConnection = function(pc) {
+        console.log("关闭PeerConnection连接");
+        if (!pc) return;
+        pc.close();
+    };
+    return new webrtc();
+};
